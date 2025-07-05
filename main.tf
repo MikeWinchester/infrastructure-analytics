a534bd592ea99ef848c495fc87041a11177e319c
Miguel Sanchez <aleminapavon30@gmail.com>
2025-07-04 19:25:39 -0600

    update

--- main.tf
+++ main.tf
@@ -10,20 +10,19 @@ resource "azurerm_resource_group" "rg" {
 
 # Data Lake Storage Account (para datos estructurados y no estructurados)
 resource "azurerm_storage_account" "datalake" {
-  name                     = "st${replace(var.project, "-", "")}${var.environment}dl"
+  name                     = "st${var.project}${var.environment}dl"
   resource_group_name      = azurerm_resource_group.rg.name
   location                = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
-  is_hns_enabled           = true # Habilita el espacio de nombres jerárquico (Data Lake)
+  is_hns_enabled           = true
 
   tags = var.tags
 }
 
 # Containers para el Data Lake
-resource "azurerm_storage_data_lake_gen2_filesystem" "containers" {
-  for_each           = toset(["raw", "processed", "curated", "sandbox"])
-  name               = each.key
+resource "azurerm_storage_data_lake_gen2_filesystem" "processed" {
+  name               = "processed"
   storage_account_id = azurerm_storage_account.datalake.id
 }
